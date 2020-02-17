19167bd4235fa656ef933667bc2e2fb580a4036a
Aura Martin <martin.aurav@gmail.com>
2020-02-17 02:52:17 +0000

    HandleOneOperation.java edited online with Bitbucket - Completed logic for handling && and ||

--- src/cpt287/infixparser/handleOneOperation/HandleOneOperation.java
+++ src/cpt287/infixparser/handleOneOperation/HandleOneOperation.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 public class HandleOneOperation {
 
-    //TODO: Complete this.  Needs to handle ||, && check to see if it works with comparators
-    // with the boolean results;
     public Integer handleOneOperation(Integer leftVal, Integer rightVal, String oper){
         Integer leftInt = leftVal;
         Integer rightInt = rightVal;
@@ -66,8 +64,19 @@ public class HandleOneOperation {
            } else {
                result = 0;
            }
+       }else if (operator.equals("&&")){
+           if(leftInt != 0 && rightInt != 0){
+               result = 1;
+           } else {
+               result = 0;          
+           }
+       }else if (operator.equals("||")){
+            if(leftInt != 0 || rightInt != 0){
+               result = 1;
+           } else {
+               result = 0;
+           }
        }
-
         return result;
     }
 }
